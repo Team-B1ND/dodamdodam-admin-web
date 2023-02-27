4f5b2ed316fbd57594494c683c7a40180cfe6c87
nrbns357 <gusgh357@gmail.com>
2023-02-27 21:05:51 +0900

    ⚡:: 시간표 수정,삭제,등록 쿼리 구현

--- src/quries/timeTable/timeTable.query.ts
+++ src/quries/timeTable/timeTable.query.ts
@@ -1,5 +1,71 @@
-import { useQuery } from "react-query";
-import timeTableRepository from "../../repositories/timeTable/timeTableRepository";
+import { AxiosError } from "axios";
+import { useMutation, useQuery, UseQueryOptions } from "react-query";
+import {
+  createTimeTableDataParam,
+  timeTableId,
+  timeTableType,
+} from "repositories/timeTable/timeTableRepository.param";
+import {
+  getTimeTablesDataResponse,
+  getTimeTableTypeResponse,
+} from "repositories/timeTable/timeTableRepository.res";
+import TimeTableRepository from "../../repositories/timeTable/timeTableRepository";
 
-export const useGetTimeTable = () =>
-  useQuery("timeTable/getTimeTable", () => timeTableRepository.getTimeTables());
+export const useGetTimeTableQuery = (
+  options?: UseQueryOptions<
+    getTimeTablesDataResponse,
+    AxiosError,
+    getTimeTablesDataResponse,
+    "timeTable/getTimeTable"
+  >
+) =>
+  useQuery(
+    "timeTable/getTimeTable",
+    () => TimeTableRepository.getTimeTables(),
+    {
+      ...options,
+      staleTime: 1000 * 60 * 60,
+      cacheTime: 1000 * 60 * 60,
+    }
+  );
+
+export const usePostCreateTimeTableMutation = () => {
+  const mutation = useMutation(
+    (createTimeTableDataParam: createTimeTableDataParam) =>
+      TimeTableRepository.postCreateTimeTable(createTimeTableDataParam)
+  );
+  return mutation;
+};
+
+export const useDeleteTimeTableMutation = () => {
+  const mutation = useMutation(({ id }: timeTableId) =>
+    TimeTableRepository.deleteTimeTable({ id })
+  );
+  return mutation;
+};
+
+export const usePatchTimeTableMutation = () => {
+  const mutation = useMutation(({ id }: timeTableId) =>
+    TimeTableRepository.patchTimeTable({ id })
+  );
+  return mutation;
+};
+
+export const useGetTimeTableType = (
+  { type }: timeTableType,
+  options?: UseQueryOptions<
+    getTimeTableTypeResponse,
+    AxiosError,
+    getTimeTableTypeResponse,
+    "timeTableType/getTimeTableType"
+  >
+) =>
+  useQuery(
+    "timeTableType/getTimeTableType",
+    () => TimeTableRepository.getTimeTableType({ type }),
+    {
+      ...options,
+      staleTime: 1000 * 60 * 60,
+      cacheTime: 1000 * 60 * 60,
+    }
+  );
