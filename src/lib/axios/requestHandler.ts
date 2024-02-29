f9a9c441aa6ed48c2645a121ceae7662e2a4136f
전해림 <HAERIM060920@gmail.com>
2024-02-29 15:48:03 +0900

    테스트

--- src/lib/axios/requestHandler.ts
+++ src/lib/axios/requestHandler.ts
@@ -1,5 +1,5 @@
 import { B1ndToast } from "@b1nd/b1nd-toastify";
-import { AxiosRequestConfig } from "axios";
+import { AxiosRequestConfig, InternalAxiosRequestConfig } from "axios";
 import {
   ACCESS_TOKEN_KEY,
   REFRESH_TOKEN_KEY,
@@ -7,14 +7,14 @@ import {
 } from "constants/token/token.constant";
 import token from "lib/token";
 
-const requestInterceptor = (config: AxiosRequestConfig) => {
+const requestInterceptor = (config: InternalAxiosRequestConfig) => {
   if (
     token.getToken(ACCESS_TOKEN_KEY) !== undefined &&
     token.getToken(REFRESH_TOKEN_KEY)
   ) {
-    config.headers = {
-      [REQUEST_TOKEN_KEY]: `Bearer ${token.getToken(ACCESS_TOKEN_KEY)}`,
-    };
+    config.headers[REQUEST_TOKEN_KEY] = `Bearer ${token.getToken(
+      ACCESS_TOKEN_KEY
+    )}`;
   } else {
     B1ndToast.showError("토큰이 존재하지 않습니다.");
     window.location.href = "/admin/sign";
