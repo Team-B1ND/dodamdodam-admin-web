6e779b18d77a9e0585378510e6f6dbff050aca5d
박상현 <104070112+Sanghyun0505@users.noreply.github.com>
2024-03-19 08:35:41 +0900

    refactor: 사용하지 않는 NumberSttingPage 삭제

--- src/components/Router/index.tsx
+++ src/components/Router/index.tsx
@@ -2,7 +2,6 @@ import withAuth from "components/HOC/withAuth";
 import AuthorityPage from "pages/Home/AuthorityPage";
 import JoinApprovalPage from "pages/Home/JoinApprovalPage";
 import MemberPage from "pages/Home/MemberPage";
-import NumberSettingPage from "pages/Home/NumberSettingPage";
 import { Route, Routes } from "react-router-dom";
 import AuthPage from "../../pages/Auth/AuthPage";
 
@@ -10,7 +9,6 @@ const Router = () => {
   const AuthJoinApprovalPage = withAuth(JoinApprovalPage);
   const AuthAuthorityPage = withAuth(AuthorityPage);
   const AuthMemberPage = withAuth(MemberPage);
-  const AuthNumberSettingPage = withAuth(NumberSettingPage);
 
   return (
     <Routes>
@@ -19,7 +17,6 @@ const Router = () => {
       <Route path="/authority" element={<AuthAuthorityPage />} />
       <Route path="/authority/:id" element={<AuthAuthorityPage />} />
       <Route path="/member" element={<AuthMemberPage />} />
-      <Route path="/numberSetting" element={<AuthNumberSettingPage />} />
     </Routes>
   );
 };
